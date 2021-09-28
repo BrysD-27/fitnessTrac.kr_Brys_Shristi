47366e8bf826f55a7e6761066a050dabb105320f
Bryson Davis <brysondavis65@gmail.com>
2021-09-27 20:30:27 -0500

    added material-ui packages

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+    BrowserRouter as Router,
+    Route, 
+    Switch
+} from 'react-router-dom';
+
+const App = () => {
+    return (
+        
+    )
+}
